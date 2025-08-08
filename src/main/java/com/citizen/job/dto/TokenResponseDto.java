d3c9cbff5cbf9d8fd5e8b22ae810e1ad9fe20023
HariHaran212 <hariharan2004121@gmail.com>
2025-08-08 08:07:54 +0530

    Modified Role

--- src/main/java/com/citizen/job/dto/TokenResponseDto.java
+++ src/main/java/com/citizen/job/dto/TokenResponseDto.java
@@ -9,6 +9,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class TokenResponseDto {
     private boolean valid;
-    private String userId;
-    private String role;
+    private String email;
+    private String message;
+//    private String userId;
+//    private String role;
 }
